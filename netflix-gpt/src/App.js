968630a2d8d07c8904ecbfddc6ab9ad6c7e219cf
rahul9369 <rp25082003@gmail.com>
2024-06-30 15:07:31 +0530

    Initial commit

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-green-300 text-center p-5 text-3xl font-bold">
+      Namaste Everyone!!!!!
+    </div>
+  );
+}
+
+export default App;
